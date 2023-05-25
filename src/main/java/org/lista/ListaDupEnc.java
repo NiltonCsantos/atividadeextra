dfc23fc919859eb9d12d1eb3bd557063dc1bb8d1
Nilton César <104584538+NiltonCsantos@users.noreply.github.com>
2023-05-25 18:10:29 -0300

    v3

--- src/main/java/org/lista/ListaDupEnc.java
+++ src/main/java/org/lista/ListaDupEnc.java
@@ -6,39 +6,8 @@ import org.no.NoDuplo;
 public class ListaDupEnc<T extends Comparable<T>> extends Lista<T>{
 
     private NoDuplo<T> inicio;
-
     private int tamanho;
 
-    public NoDuplo<T> getInicio() {
-        return inicio;
-    }
-
-    public void setInicio(NoDuplo<T> inicio) {
-        this.inicio = inicio;
-    }
-
-    public void setTamanho(int tamanho) {
-        this.tamanho = tamanho;
-    }
-
-    @Override
-    public String toString() {
-
-        String listaCompleta="";
-
-        NoDuplo<T> aux= this.inicio;
-
-        for (int i = 0; i <this.tamanho ; i++) {
-
-            listaCompleta+=aux.dado+"\t";
-
-            aux=aux.prox;
-
-        }
-
-        return  listaCompleta;
-
-    }
 
     @Override
     public void Lista() {
@@ -54,9 +23,10 @@ public class ListaDupEnc<T extends Comparable<T>> extends Lista<T>{
             this.inicio=novoDado;
             this.tamanho++;
         }else{
+
             NoDuplo<T> aux= this.inicio;
 
-             for (int i = 0; i <this.tamanho-1 ; i++) {
+            for (int i = 0; i <this.tamanho-1 ; i++) {
 
                 aux=aux.prox;
 
@@ -73,52 +43,53 @@ public class ListaDupEnc<T extends Comparable<T>> extends Lista<T>{
     @Override
     public void incluirInicio(T elemento) throws Exception {
 
+        NoDuplo <T> dado= new NoDuplo<>(elemento);
+
+        dado.prox=this.inicio;
+
+        this.inicio.ant=dado;
+
+        this.inicio=dado;
+
+        this.tamanho++;
+
     }
 
     @Override
     public void incluir(T elemento, int posicao) throws Exception {
 
-        NoDuplo<T> aux= new NoDuplo<>(elemento);
 
-        if(posicao==0 && this.tamanho==0){
 
-            this.inicio=aux;
-            setTamanho(this.tamanho+1);
+        if(this.inicio==null){
+
+            incluir(elemento);
 
         } else if (posicao==0 && this.tamanho>0) {
 
-            aux.prox=this.inicio;
+            incluirInicio(elemento);
 
-            this.inicio.ant=aux;
-
-            this.inicio=aux;
-
-            setTamanho(this.tamanho+1);
-
-        } else  if (posicao>=this.tamanho || posicao<0){
+        }else  if (posicao>=this.tamanho || posicao<0){
 
             throw new Exception("posição inválida!");
 
-        }else{
+        }  else{
+            NoDuplo<T> aux= new NoDuplo<>(elemento);
 
             NoDuplo<T> novoDado= new NoDuplo<>(elemento);
 
             NoDuplo<T> anterior= null;
 
-
             aux=this.inicio;
 
-            for (int i = 0; i<posicao ; i++) {
-
-                if (i==posicao-1){
-
-                    anterior=aux;
-
-                }
+            for (int i = 0; i<posicao-1 ; i++) {
 
                 aux=aux.prox;
 
             }
+
+            anterior=aux;
+
+            aux=aux.prox;
 
             anterior.prox= novoDado;
             novoDado.ant=anterior;
@@ -129,8 +100,6 @@ public class ListaDupEnc<T extends Comparable<T>> extends Lista<T>{
             this.tamanho++;
 
         }
-
-
 
     }
 
@@ -167,47 +136,55 @@ public class ListaDupEnc<T extends Comparable<T>> extends Lista<T>{
     @Override
     public Lista<T> subLista(int posInicial, int posFinal) throws Exception {
 
-        NoDuplo<T> aux= this.inicio;
-        int count=0;
+        if (posInicial>= this.tamanho || posFinal>= this.tamanho){
+            throw new Exception("indíce inválido!");
+        }
+
+        if (posInicial<0 || posFinal<0){
+            throw new Exception("indíce inválido!");
+        }
 
         Lista listaAuxliar = new ListaDupEnc();
 
-        for (int i = 0; i <posInicial ; i++) {
+        NoDuplo<T> aux= this.inicio;
 
-            aux=aux.prox;
+
+        if (posInicial>posFinal) {
+
+            for (int i = 0; i <posInicial ; i++) {
+                aux=aux.prox;
+            }
+
+            for (int i = posInicial; i >=posFinal ; i--) {
+
+                listaAuxliar.incluir(aux.dado);
+                aux=aux.ant;
+
+            }
+
+
+        }else{
+
+            for (int i = 0; i <posInicial ; i++) {
+
+                aux=aux.prox;
+
+            }
+
+
+            for (int i = posInicial; i <=posFinal ; i++) {
+
+                listaAuxliar.incluir(aux.dado);
+                aux=aux.prox;
+
+            }
 
         }
-
-
-
-        for (int i = posInicial; i <=posFinal ; i++) {
-
-            listaAuxliar.incluir(aux.dado);
-            aux=aux.prox;
-
-        }
-
 
         return listaAuxliar;
     }
 
-    public void listarContrario(){
-
-        NoDuplo<T> aux= this.inicio;
-
-        for (int i = 0; i <this.tamanho-1; i++) {
-
-            aux=aux.prox;
-
-        }
-
-
-        for (int i = this.tamanho; i >0 ; i--) {
-
-            System.out.print(aux.dado+"\t");
-            aux=aux.ant;
-
-        }
-    }
 }
+
+
 
